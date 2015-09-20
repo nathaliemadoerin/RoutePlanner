ebb57e9eb52e9b649d3032e542e376c1342f7d03
nathaliemadoerin <nathalie.madoerin@students.fhnw.ch>
2015-09-20 16:43:39 +0200

    Creation of the Project

---
+++ RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -0,0 +1,19 @@
+﻿using Fhnw.Ecnf.RoutePlanner.RoutePlannerLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RoutePlannerConsole {
+    class RoutePlannerConsoleApp {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to RoutePlanner (Version {0})", Assembly.GetExecutingAssembly().GetName().Version);
+            var wayPoint = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312);
+            Console.WriteLine("{0}: {1}/{2}", wayPoint.Name, wayPoint.Latitude, wayPoint.Longitude);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RoutePlannerLib/WayPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib {
+    public class WayPoint {
+        public string Name { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public WayPoint(string _name, double _latitude, double _longitude) {
+                Name = _name; Latitude = _latitude; Longitude = _longitude;
+        }
+    }
+}
